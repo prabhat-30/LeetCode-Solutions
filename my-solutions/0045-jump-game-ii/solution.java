0ab2ca09070fc9a9a2bf8843c74065975191703a
Prabhat Kumar <127616793+prabhat-30@users.noreply.github.com>
2024-01-23 05:41:27 +0000

    Sync LeetCode submission Runtime - 1 ms (99.73%), Memory - 44.7 MB (99.95%)

---
+++ my-solutions/0045-jump-game-ii/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Solution {
+
+    int jump(int []nums) {
+
+        int jumps=0;
+        int current=0;
+        int farthest=0;
+        for (int i = 0; i < nums.length-1; i++) {
+           farthest =Math.max(farthest,nums[i]+i);
+
+           if(i==current){
+               current = farthest;
+               jumps++;
+           }
+        }
+        
+        return jumps;
+    }
+}
+
