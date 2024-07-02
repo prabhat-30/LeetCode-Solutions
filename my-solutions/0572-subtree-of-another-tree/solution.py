eff232da2eeca07e1590ee15b6024a890205ae0f
Prabhat Kumar <127616793+prabhat-30@users.noreply.github.com>
2024-07-02 08:52:15 +0000

    Sync LeetCode submission Runtime - 43 ms (44.09%), Memory - 16.7 MB (100.00%)

---
+++ my-solutions/0572-subtree-of-another-tree/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        def convert(p):
+            return "^" + str(p.val) + "#" + convert(p.left) + convert(p.right) if p else "$"
+        
+        return convert(t) in convert(s)
